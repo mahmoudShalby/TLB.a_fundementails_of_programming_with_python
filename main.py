91defdf2484e1d72b0e28d6f6d13a2422fa8be60
Mahmoud Shalaby <mhmooud074@gmail.com>
2023-05-20 23:31:46 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+'''
+lecture 1:
+  1. print function
+  2. type function
+  3. comments
+'''
+# 1.1 print function
+print(5) # this line prints number 5
+print(4.2) # this line prints number 4.2
+print('this is a string')
+print("this is a string")
+print(True)
+print(False)
+
+# 1.2 type function
+# int(integer) = type(5)
+print(int)
+print(type(5))
+print(type('Welcome to TLB'))
+print(type("Welcome to TLB"))
+print(type(True))
+print(type(False))
+
+# 1.3 comments
+# print('hello Ali, Monzer and Wael')
+# print("hello Ali, Masdfsssssssssssafhaslkdfhashdfhadonzer and Wael")
+# print("hello Ali, Masdfsssssssssssafhaslkdfhashdfhadonzer and Wael")
+
+'''
+print('hello Ali, Monzer and Wael')
+print("hello Ali, Masdfsssssssssssafhaslkdfhashdfhadonzer and Wael")
+print("hello Ali, Masdfsssssssssssafhaslkdfhashdfhadonzer and Wael")
+'''
+
+"""
+print('hello Ali, Monzer and Wael')
+print("hello Ali, Masdfsssssssssssafhaslkdfhashdfhadonzer and Wael")
+print("hello Ali, Masdfsssssssssssafhaslkdfhashdfhadonzer and Wael")
+"""
